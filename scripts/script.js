5b13b064bea6c69fea73978ddbe1ee4f10791756
Croke <jorgecuadradosaez@gmail.com>
2020-01-10 18:35:01 +0100

    new ios test

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,17 @@ var video = document.getElementById("bgVideo");
 if (video.paused) {
   video.play();
 }
+
+var music = document.getElementById("musiclistener");
+music.addEventListener("mousemove", function(){
+  var audio = document.getElementById("player");
+  audio.play();
+});
+
+function fallback(video)
+{
+  var img = video.querySelector('img');
+  if (img) {
+    video.parentNode.replaceChild(img, video);
+  }
+}
