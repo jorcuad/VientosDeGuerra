a971ddb57a1e0d44ac6c59837bc034bc5407e247
Croke <jorgecuadradosaez@gmail.com>
2020-01-10 18:27:19 +0100

    new ios test

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,4 @@
-// Pause and play the video, and change the button text
-function myFunction() {
-  // Get the video
-  var video = document.getElementById("myVideo2");
-
-  // Get the button
-  var btn = document.getElementById("myBtn");
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
+var video = document.getElementById("bgVideo");
+if (video.paused) {
+  video.play();
 }
-
-var music = document.getElementById("musiclistener");
-music.addEventListener("mousemove", function(){
-  var audio = document.getElementById("player");
-  audio.play();
-});
-
-music.on('click touchstart', function () {
-        const videoElement = document.getElementById('myVideo2');
-        if (videoElement.playing) {
-            // video is already playing so do nothing
-        }
-        else {
-            // video is not playing
-            // so play video now
-            videoElement.play();
-        }
-});
