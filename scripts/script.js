053be3a233e703af311efa1d8bc86d32e58ce112
Croke <jorgecuadradosaez@gmail.com>
2020-01-10 18:14:07 +0100

    check play in apple

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 // Pause and play the video, and change the button text
 function myFunction() {
   // Get the video
-  var video = document.getElementById("myVideo");
+  var video = document.getElementById("myVideo2");
 
   // Get the button
   var btn = document.getElementById("myBtn");
@@ -18,4 +18,16 @@ var music = document.getElementById("musiclistener");
 music.addEventListener("mousemove", function(){
   var audio = document.getElementById("player");
   audio.play();
+});
+
+music.on('click touchstart', function () {
+        const videoElement = document.getElementById('myVideo2');
+        if (videoElement.playing) {
+            // video is already playing so do nothing
+        }
+        else {
+            // video is not playing
+            // so play video now
+            videoElement.play();
+        }
 });
