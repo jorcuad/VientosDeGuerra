49f53e9fb84fb67fe520dcbc3be49c3de821863f
Croke <jorgecuadradosaez@gmail.com>
2020-01-10 18:55:37 +0100

    more strings for markov

--- scripts/markov.js
+++ scripts/markov.js
@@ -1,6 +1,6 @@
 // source: https://en.wikipedia.org/wiki/Category:The_Cure_songs
 
-let input =     `Time is money, friend!
+let input = `Time is money, friend!
     Spit it out!
     I got the best deals, anywhere!
     I got what you need!
@@ -12,7 +12,7 @@ let input =     `Time is money, friend!
     Be good!
     Be careful out there.
     Hm, interesting.
-    Heeey, how you doin'?
+    Heeey, how you?
     Yeah, very funny...
     Alright, move along now!
     I gota long day kid...
@@ -20,16 +20,63 @@ let input =     `Time is money, friend!
     You're just embarrassing yourself...
     I don't have time for this...
     You, knock it off!
-    Yo
     Heh, heh, Glad I could help!
     Amateur!!
     Keep it real!
-    GTL friend; gambling, tinkering, laundry. Note: has been debated, laundering, launderin'. jersey shore ref
     Can i lighten that coin purse up for ya?.
     Cha-ching!
     You break it, you buy it.
     I ain't got it, you don't want it.
-    Can I interest you on a payment plan?`;
+    Can I interest you on a payment plan?
+    Time is money, friend!
+    Well, spit it out!
+    I got the best deals, anywhere!
+    I got what you need!
+    Ah, potential customer.
+    May I show you my wares?
+    Welcome, friend!
+    Yeah, what do you want?
+    Hey, how you?
+    Wazzup?
+    Hm, interesting
+    Be good!
+    Be careful out there.
+    Keep it real!
+    Hurry back!
+    Ello.
+    Don't be shy.
+    Greetings.
+    Talk to me.
+    Who you be?
+    How you ?
+    You come get da Voodoo.
+    Relax.
+    Hello!
+    What be on your mind?
+    Later.
+    Spirits be with you.
+    Be seeing ya.
+    Stay away from the Voodoo.
+    Me got the good stuff.
+    Be coo!
+    Making me angry, mon.
+    The spirit not amused.
+    Okay dokay
+    Taz'dingooo!
+    Don't be shy.
+    Hey dere!
+    How are ya?
+    Talk to me.
+    Hello!
+    You come get the Voodoo.
+    Relax.
+    What'cha looking for?
+    Me got the good stuff.
+    Be good
+    Be seeing ya.
+    Okay dokay
+    Stay away from the Voodoo.
+    You be careful` ;
 
 input = input.split('\n');
 var dialog = document.getElementById("dialog-text");
